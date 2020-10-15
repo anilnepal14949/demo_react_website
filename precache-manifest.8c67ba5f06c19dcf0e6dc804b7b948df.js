24415d6dcf2cbe80990f82ea8aa3f790a3ccc145
Anil Nepal <anilnepal14949@gmail.com>
2020-10-15 23:02:08 +1100

    Updates

---
+++ precache-manifest.8c67ba5f06c19dcf0e6dc804b7b948df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6f5dcda7c837ef21495b1748343f28",
+    "url": "/demo_react_website/index.html"
+  },
+  {
+    "revision": "c53269488b5edf4539e8",
+    "url": "/demo_react_website/static/css/main.4649ade8.chunk.css"
+  },
+  {
+    "revision": "b1df5137cf1be4301865",
+    "url": "/demo_react_website/static/js/2.062b75ab.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/demo_react_website/static/js/2.062b75ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c53269488b5edf4539e8",
+    "url": "/demo_react_website/static/js/main.f97dd70c.chunk.js"
+  },
+  {
+    "revision": "e79379288bd951b2f302",
+    "url": "/demo_react_website/static/js/runtime-main.c23e4b2e.js"
+  }
+]);
